895e3730a2baa34bee324f88a7b265abe6d31c8a
Aaron LeCheminant <Aaron@Brelyns-MacBook-Pro-2.local>
2017-10-11 10:53:35 -0400

    update FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+   state = {
+      people: []
+   }
+   componentDidMount(){
+      axios.get('http://localhost:8080/')
+         .then(response => {
+            console.log(response);
+            this.setState({people: response.data })
+         })
+   }
+
+   render() {
+      return (
+         <div className="container">
+            <ul>
+               {this.state.people.map(person => {
+                  return (
+                     <li key={person.id}>
+                        {person.firstName}
+                     </li>
+                  );
+               })}
+            </ul>
+         </div>
+      );
+   }
 }
 
 export default App;

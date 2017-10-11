8b53efc9e3e9b74a402f3aace1451082e48b36cd
Calvin Webster <calweb@gmail.com>
2017-10-11 11:10:51 -0400

    finally got the right folder in there

--- backend/src/main/java/org/theironyard/hackathon/PersonController.java
+++ backend/src/main/java/org/theironyard/hackathon/PersonController.java
@@ -1,12 +1,11 @@
 package org.theironyard.hackathon;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 public class PersonController {
 
@@ -16,5 +15,13 @@ public class PersonController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public List<Person> getPeople() {
         return personService.findAll();
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    public void addPerson(@RequestBody Person newPerson) {
+        System.out.println(newPerson.getFirstName());
+
+        personService.add(newPerson);
+
     }
 }
